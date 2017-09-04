d09d7b64494332bd4968fda5a0934623c348afc7
shamanoff <shamanoff@gmail.com>
2017-09-04 18:18:57 +0300

    home

--- room-services/src/main/java/com/micro/roomservices/RoomController.java
+++ room-services/src/main/java/com/micro/roomservices/RoomController.java
@@ -1,5 +1,7 @@
 package com.micro.roomservices;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rooms")
+@Api(value = "rooms", description = "Data service operations on rooms", tags = ("rooms"))
 public class RoomController {
     @Autowired
     private RoomRepo roomRepo;
 
     @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Get All Rooms", notes = "Get", nickname = "getRooms")
     public List<Room> findAll(@RequestParam(name = "roomNumber", required = false)String roomNumber){
         if(StringUtils.isNotEmpty(roomNumber)){
             return Collections.singletonList(this.roomRepo.findByRoomNumber(roomNumber));
